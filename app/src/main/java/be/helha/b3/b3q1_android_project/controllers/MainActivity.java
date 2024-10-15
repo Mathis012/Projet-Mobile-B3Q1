33be6c829940bd3f0363422740245fb90cdb40b3
Mathis <la228315@student.helha.be>
2024-10-15 09:52:46 +0200

    Merge

--- app/src/main/java/be/helha/b3/b3q1_android_project/controllers/MainActivity.java
+++ app/src/main/java/be/helha/b3/b3q1_android_project/controllers/MainActivity.java
@@ -1,6 +1,9 @@
 package be.helha.b3.b3q1_android_project.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton evaluationButton = findViewById(R.id.evaluationButton);
+        evaluationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EvaluationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton studentButton = findViewById(R.id.studentButton);
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
