0cde44ca703bd95ef81cf1167438d16df119a13c
Lyara <la227988@student.helha.be>
2024-10-15 09:32:19 +0200

    Visuel page classes + page étudiants + page évaluations

--- app/src/main/java/be/helha/b3/b3q1_android_project/MainActivity.java
+++ app/src/main/java/be/helha/b3/b3q1_android_project/MainActivity.java
@@ -1,6 +1,9 @@
 package be.helha.b3.b3q1_android_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton evaluationButton = findViewById(R.id.evaluationButton);
+        evaluationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EvaluationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton studentButton = findViewById(R.id.studentButton);
+        studentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StudentActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
